8b19c351cbca9068f23d42d1bd061b1b4b70c518
Cristina Zhang <rami0141@umn.edu>
2018-02-04 22:05:51 -0600

    more code

--- liri.js
+++ liri.js
@@ -1,16 +1,68 @@
+//dependencies
 require("dotenv").config();
 
 // fs is a core Node package for reading and writing files
 var fs = require("fs");
+var keys = require('./keys.js')
 var twitter = require("twitter");
 var request = require("request");
 var spotify = require("node-spotify-api");
 
+// Take two arguments.
+var action = process.argv[2];
+var nodeArgs = process.argv
+var input = "";
 
-//keeping credentials private
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+// LOOPS THROUGH ALL THE WORDS IN THE NODE ARGUMENT 
+// DO A LOOP TO GET EVERYTHING AFTER THE INDEX OF 2 NODE ARGUMENT
+for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        input = input + "+" + nodeArgs[i];
+    } else {
+        input = input+ nodeArgs[i];
+    }
+}
+
+
+// This will tell which function will run
+switch (action) {
+	case 'my-tweets':
+	showTweets();
+	break;
+
+	case `spotify-this-song`:
+	showSpotify();
+	break;
+
+	case `movie-this`:
+	showMovie();
+	break;
+
+	case `do-what-it-says`:
+	showFs();
+	break;
+
+	default:
+    console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
+  	break;
+}
+
+// twitter action  -- figure out later
+function showTweets() {
+
+}
+
+// Spotify action
+
+function showSpotify() {
+}
+
+// Movie action
+function showMovie() {
+
+}
+
+// fs
+function showFs() {
+
+}
