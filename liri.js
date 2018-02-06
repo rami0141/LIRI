d9a2a66c64adc4d9b860243ee05229d226366ece
Cristina Zhang <rami0141@umn.edu>
2018-02-05 21:19:30 -0600

    showMovie code

--- liri.js
+++ liri.js
@@ -9,14 +9,15 @@ var request = require("request");
 var Spotify = require("node-spotify-api");
 
 var action = process.argv[2];
-var nodeArgs = process.argv[3];
+//var nodeArgs = process.argv[3];
 var input = "";
-
-console.log(nodeArgs);
-console.log(action);
 
 // LOOPS THROUGH ALL THE WORDS IN THE NODE ARGUMENT 
 // DO A LOOP TO GET EVERYTHING AFTER THE INDEX OF 2 NODE ARGUMENT
+//for (var i = 3; i < nodeArgs.length; i++) {
+//	input = input + " " + nodeArgs[i];
+//}
+
 // for (var i = 3; i < nodeArgs.length; i++) {
 //     if (i > 3 && i < nodeArgs.length) {
 //         input = input + "+" + nodeArgs[i];
@@ -48,15 +49,14 @@ switch (action) {
   	break;
 }
 
-// twitter action  -- figure out later
-
+// twitter action 
 function showTweets() {
-	//validatiom
-	var T = new Twitter(keys.twitter);
+	// Linking API key for Twitter
+	var client = new Twitter(keys.twitter);
 
-  	var params = {screen_name: 'crami0141', count: 10};
+  	var params = {screen_name: 'crami0141', count: 20};
 
-  	T.get('statuses/user_timeline', params, function(error, tweets, response) {
+  	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
     if (!error) {
       for (var i = 0; i < tweets.length; i++) {
@@ -65,20 +65,79 @@ function showTweets() {
       } 
   	};
   });
+ // This ends the showTweets function	 	
  }
-
+//------------------------------------------------------------------------------------------------
 
 // Spotify action
-// validatiom
-//	var S = new Spotify(keys.switter);
-//function showSpotify() {
+// function showSpotify() {
+// 	// Linking API key for Spotify
+// 	var spotify = new Spotify(keys.spotify);
+
+// 	var song = process.argv[3];
+
+// 	spotify.search({ type: 'track', query: 'query.song_title' }, function(err, data) {
+//   	if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+//
+//logThis('Artist Name: ' + data.tracks.items[0].artists[0].name);
+//		logThis('Song Name: ' + data.tracks.items[0].name);
+//		logThis('Preview Link: ' + data.tracks.items[0].preview_url);
+//		logThis('Album Title: ' + data.tracks.items[0].album.name);
+// console.log(data); 
+// });
+// This ends the showSpotify function	
 // }
 
-// // Movie action
-// function showMovie() {
+//--------------------------------------------------------------------------------------------
+// Movie action
+function showMovie() {
+	// Grab the movieName
+	var nodeArgs = process.argv;
+	var movieName = process.argv[3];
 
+// 	for (var i = 3; i < nodeArgs.length; i++) {
+
+//  	 if (i > 3 && i < nodeArgs.length) {
+// 	movieName = movieName + "+" + nodeArgs[i];
+// } else {
+//     movieName += nodeArgs[i];
+//   }
 // }
+	// Running a request using omdb api
+	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
+	console.log(queryUrl);
+	request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // console logging the following info 
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Release Year: " + JSON.parse(body).Year);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+    console.log("Country Produced: " + JSON.parse(body).Country);
+    console.log("Language: " +  JSON.parse(body).Language);
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+  }
+});
+// This ends the showMovie function	
+}
+
+ // * Title of the movie.
+ //   * Year the movie came out.
+ //   * IMDB Rating of the movie.
+ //   * Rotten Tomatoes Rating of the movie.
+ //   * Country where the movie was produced.
+ //   * Language of the movie.
+ //   * Plot of the movie.
+ //   * Actors in the movie.
+
+//---------------------------------------------------------------------------------------------------
 // // fs
 // function showFs() {
 
